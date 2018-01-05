cc5325b05a8ae43336ae2b1ddd53253b09d56557
Juan Sierra <jasierra.garcia@gmail.com>
2018-01-04 23:02:50 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// 1. define variables
+// 2. add event listeners
+// 3. set the size of the cross stitch canvas
+
+function makeGrid() {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+}
