56ac062fdab7cfde40c50323316b4c5ec5ceadfa
Juan Sierra <jasierra.garcia@gmail.com>
2018-01-09 18:05:53 -0500

    Save and retrive input data to and from local storage

--- js/main.js
+++ js/main.js
@@ -2,7 +2,26 @@
 // 2. add event listeners
 // 3. set the size of the cross stitch canvas
 
-function makeGrid() {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
+let height = document.getElementById("input_height");
+let width = document.getElementById("input_width");
+
+function sizePicker() {
+  window.onsubmit = sizePicker;
 }
+
+function saveSize() {
+  localStorage.setItem("height", height.value);
+  localStorage.setItem("width", width.value);
+
+  console.log(localStorage.getItem(height));
+}
+
+function retriveData() {
+  height.value = localStorage.getItem("height");
+  width.value = localStorage.getItem("width");
+  console.log(height.value, width.value);
+}
+
+retriveData();
+
+window.addEventListener("click", sizePicker, false);
